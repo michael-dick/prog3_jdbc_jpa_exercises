2499ee109fc94e5b8200cf786418c856d57871f5
Michael Dick <michaeldick94@web.de>
2015-11-01 13:59:02 +0100

    imported project

---
+++ jdbc_jpa_demo/src/main/java/prog3/jdbc_jpa_demo/App.java
@@ -0,0 +1,13 @@
+package prog3.jdbc_jpa_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
